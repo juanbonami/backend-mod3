09751611a4c25cc63e86c1c63175d43a07848688
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 01:34:48 -0500

    created first model with private instance variables and setters and getters

--- src/main/java/com/example/news2/models/Comment.java
+++ src/main/java/com/example/news2/models/Comment.java
@@ -1,4 +1,57 @@
 package com.example.news2.models;
 
+import javax.persistence.*;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "comment_name")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String subject;
+
+    @Column
+    private int release;
+
+    public Comment() { }
+
+    public void setBookid() {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setRelease(int release) {
+        this.release = release;
+    }
+
+    public int getRelease() {
+        return release;
+    }
+
 }
