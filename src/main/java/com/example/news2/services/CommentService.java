71032065d034e64d878e4c87eef379b8542feb8d
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 01:39:37 -0500

    added create Instance of comment method and list every comment created method

--- src/main/java/com/example/news2/services/CommentService.java
+++ src/main/java/com/example/news2/services/CommentService.java
@@ -1,4 +1,9 @@
 package com.example.news2.services;
 
+import com.example.news2.models.Comment;
+
 public interface CommentService {
+    public Comment createComment(Comment comment);
+
+    public Iterable<Comment> listComment();
 }
