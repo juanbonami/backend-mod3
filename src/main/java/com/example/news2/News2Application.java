f53dd1a2756367a2d40eeed1aef4073bb9b67f3d
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 00:47:25 -0500

    init

---
+++ src/main/java/com/example/news2/News2Application.java
@@ -0,0 +1,13 @@
+package com.example.news2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class News2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(News2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/news2/News2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class News2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
