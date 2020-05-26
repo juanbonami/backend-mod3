e346d17f5b2db59d1ace3b265bf2d4b06b27f93a
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 01:21:15 -0500

    added request mapping endpoint

--- src/main/java/com/example/news2/controller/ControllerThree.java
+++ src/main/java/com/example/news2/controller/ControllerThree.java
@@ -1,4 +1,12 @@
 package com.example.news2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
 public class ControllerThree {
+    
 }
