8334b01b779f71cc37bfcb587e5c657dc8c89797
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 01:12:52 -0500

    added method refrencing get mapping

--- src/main/java/com/example/news2/controller/ControllerTwo.java
+++ src/main/java/com/example/news2/controller/ControllerTwo.java
@@ -1,4 +1,14 @@
 package com.example.news2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class ControllerTwo {
+    @GetMapping
+    public String exampleOne() {
+        return "first end point";
+    }
 }
