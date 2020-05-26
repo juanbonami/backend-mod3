c7af3e43e8b30666149f881edc052863b6b01ccb
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 02:03:19 -0500

    implemented methods from CommentService Interface and polymorphized it's methods or gave it behavior in our CommentServiceImpl class

--- src/main/java/com/example/news2/repository/CommentRepository.java
+++ src/main/java/com/example/news2/repository/CommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-    
+
 }
--- src/main/java/com/example/news2/services/CommentServiceImpl.java
+++ src/main/java/com/example/news2/services/CommentServiceImpl.java
@@ -1,9 +1,23 @@
 package com.example.news2.services;
 
+import com.example.news2.models.Comment;
+import com.example.news2.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CommentServiceImpl {
+public class CommentServiceImpl implements CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public Comment createComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public Iterable<Comment> listComment() {
+        return commentRepository.findAll();
+    }
 
 }
