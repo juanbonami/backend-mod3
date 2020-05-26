6c544e1879c46e41e17ba44b94bcc7daebb9f388
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 01:53:45 -0500

    extended the crudRepo from spring framework

--- src/main/java/com/example/news2/repository/CommentRepository.java
+++ src/main/java/com/example/news2/repository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.example.news2.repository;
 
-public interface CommentRepository {
+import com.example.news2.models.Comment;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+    
 }
