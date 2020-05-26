fca8e778f0714eba8c952fcf8bdc4d2fea84dc9f
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 01:46:54 -0500

    added get mapping with Iterable method and post mapping method

--- src/main/java/com/example/news2/controller/ControllerThree.java
+++ src/main/java/com/example/news2/controller/ControllerThree.java
@@ -1,12 +1,27 @@
 package com.example.news2.controller;
 
+import com.example.news2.models.Comment;
+import com.example.news2.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comments")
 public class ControllerThree {
+    @Autowired
+    CommentService commentService;
 
+    @GetMapping
+    public Iterable<Comment> listComment() {
+        return commentService.listComment();
+    }
+
+
+    @PostMapping
+    public Comment createComment(@RequestBody Comment comment) {
+        return commentService.createComment(comment);
+    }
 }
